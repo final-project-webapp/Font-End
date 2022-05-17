b247cbe4ade2aae27e7075a40aba60fbdb290cd0
Kamolwish Woramethaleot <kamolwish.riew189521@mail.kmutt.ac.th>
2022-05-17 13:43:48 +0700

    Edit: Member.vue

--- pages/member.vue
+++ pages/member.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="bg-zinc-800 min-h-screen">
-    
+    <SlideBar class="pl-20 pt-10"/>
 
     <MemberList class="pt-20" />
   </div>
 </template>
 
 <script>
-
+import SlideBar from '@/components/slide-bar.vue'
 import MemberList from '@/components/MemberList.vue'
 export default {
   name: 'MemberPage',
   components: {
-    MemberList
+    MemberList,
+    SlideBar
   },
   layout: "nav-bar"
 }
